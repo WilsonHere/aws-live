9cac961b2f2d96e951b93af7e9bcfd25880eedb9
WilsonHere <164607078+WilsonHere@users.noreply.github.com>
2024-04-16 16:12:23 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customhost = "database-2.cpsiuu6cwpxh.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
-custompass = "Bait3273"
+custompass = "Aacs3253"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
+custombucket = "kamwilson-employee"
 customregion = "us-east-1"
 
